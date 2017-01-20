756e91050e9769a59b4dd3c3bbea1994cd066622
drons <drons@list.ru>
2017-01-20 11:22:08 +0300

    Add project files

---
+++ start.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+make
+#./gen_RMAT -s 3 &&
+#./solution -in rmat-3 &&
+#./validation -in rmat-3 -res rmat-3.res &&
+
+#./gen_RMAT -s 4 &&
+#./solution -in rmat-4 &&
+#./validation -in rmat-4 -res rmat-4.res &&
+
+#./gen_RMAT -s 5 &&
+#./solution -in rmat-5 &&
+#./validation -in rmat-5 -res rmat-5.res &&
+
+#./gen_RMAT -s 6 &&
+#./solution -in rmat-6 &&
+#./validation -in rmat-6 -res rmat-6.res &&
+
+#./gen_RMAT -s 7 &&
+#./solution -in rmat-7 &&
+#./validation -in rmat-7 -res rmat-7.res &&
+
+#./gen_RMAT -s 8 &&
+#./solution -in rmat-8 &&
+#./validation -in rmat-8 -res rmat-8.res &&
+
+#./gen_RMAT -s 9 &&
+#./solution -in rmat-9 &&
+#./validation -in rmat-9 -res rmat-9.res &&
+
+#./gen_RMAT -s 10 &&
+#./solution -in rmat-10 &&
+#./validation -in rmat-10 -res rmat-10.res &&
+
+./gen_RMAT -s 11 &&
+./solution -in rmat-11 &&
+#./validation -in rmat-11 -res rmat-11.res &&
+
+#./gen_RMAT -s 12 &&
+#./solution -in rmat-12 &&
+#./validation -in rmat-12 -res rmat-12.res &&
+
+#./gen_RMAT -s 16 &&
+#./solution -in rmat-16 &&
+#./validation -in rmat-16 -res rmat-16.res &&
+echo "DONE!"
