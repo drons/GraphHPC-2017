a104d532457cac1e4b020f9f1bc7c32261667662
drons <drons@list.ru>
2017-03-04 12:22:14 +0300

    Add travis CI build

---
+++ run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for GS in 4 5 6 7 8 9 10; do
+	./gen_RMAT -s $GS &&
+	./solution -nIters 1 -in rmat-$GS &&
+	./gen_valid_info -in rmat-$GS &&
+	./validation -ans rmat-$GS.ans -res rmat-$GS.res &&
+	./gen_random -s $GS &&
+	./solution -nIters 1 -in random-$GS &&
+	./gen_valid_info -in random-$GS &&
+	./validation -ans random-$GS.ans -res random-$GS.res ||
+	exit 1
+done
+
+exit 0
