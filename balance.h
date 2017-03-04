12eb186478b182be1a9a5811af277481459e4596
drons <drons@list.ru>
2017-03-04 12:04:40 +0300

    Move graph sort type to balance.h

--- balance.h
+++ balance.h
@@ -1,6 +1,6 @@
-#define BFS_LEVEL_A 3.5
+#define BFS_LEVEL_A 4
 #define BFS_LEVEL_B 1
 #define BC_LEVEL_A 1
-#define BC_LEVEL_B 3.5
+#define BC_LEVEL_B 4
 #define MAX_QUEUE_LEN 0
-#define GSORT_ORDER -1
+#define GSORT_ORDER 0
--- solution_mpi.cpp
+++ solution_mpi.cpp
@@ -1,4 +1,5 @@
 #include "solution.h"
+#include "balance.h"
 
 vertex_id_t get_local_vert_count( vertex_id_t TotVertices, unsigned size, unsigned rank )
 {
@@ -102,7 +103,7 @@ void run_mpi( graph_t* g_local, double* result )
     compute_buffer_t*               buffers;
     std::vector<uint32_t>           rows_indices32;
     int                             max_work_threads = omp_get_max_threads();
-    std::vector< vertex_id_t>       map( sort_graph( G, G, 0 ) );
+    std::vector< vertex_id_t>       map( sort_graph( G, GSORT_ORDER ) );
 
     rows_indices32.resize( G->n + 1 );
     for( size_t n = 0; n < G->n + 1; ++n )
